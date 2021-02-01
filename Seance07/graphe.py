e6abb5da027ec0c207d5f78a3b37051aa5d4d727
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2021-02-01 11:02:22 +0100

    MAJ Seance 7

---
+++ Seance07/graphe.py
@@ -0,0 +1,62 @@
+"""Description.
+
+Librairie pour le parcours graphe.
+"""
+from collections import deque
+from typing import List, Tuple
+
+Sommet = int
+
+class Graphe_v1:
+    """Utilise la définition mathématique."""
+    def __init__(self, sommets: List[Sommet], arretes: List[Tuple[Sommet, Sommet]]):
+        self._sommets = sommets
+        self._arretes = arretes
+        
+    def __getitem__(self, sommet: Sommet):
+        """Itère sur les voisins du sommet."""
+        ...
+        
+class Graphe_v2:
+    """Utilise un dictionnaire de voisinage sommet -> voisins"""
+    def __init__(self, voisinage: Dict[Sommet, List[Sommet]]):
+        self._voisinage = voisinage
+        
+    def __getitem__(self, sommet):
+        """Itère sur les voisins du sommet."""
+        ...
+
+class Graphe_v3:
+    """Utilise la représentation matricielle."""
+    def __init__(self, ):
+        ...
+        
+    def __getitem__(self, sommet):
+        """Itère sur les voisins du sommet."""
+        ...
+    
+    
+def dfs(depart, graphe):
+    """Itèrateur sur les sommets du graphe en partant de depart."""
+    deja_visites = set()
+    a_visites = deque([depart])
+    while a_visites:
+        sommet_courant = a_visites.pop()
+        if sommet_courant not in deja_visites:
+            yield sommet_courant
+            deja_visites.add(sommet_courant)
+            for voisin in graphe[sommet_courant]:
+                a_visites.appendleft(voisin)
+            
+    
+def bfs(depart, graphe):
+    """Itèrateur sur les sommets du graphe en partant de depart."""
+    deja_visites = set()
+    a_visites = deque([depart])
+    while a_visites:
+        sommet_courant = a_visites.pop()
+        if sommet_courant not in deja_visites:
+            yield sommet_courant
+            deja_visites.add(sommet_courant)
+            for voisin in graphe[sommet_courant]:
+                a_visites.append(voisin)
