7e99d8856439378ee5d4214facb19b88de00a094
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2021-02-03 11:42:05 +0100

    Ajoute auxiliaire.py dans Seance 08

---
+++ Seance08/graphe_p.py
@@ -0,0 +1,15 @@
+"""Description.
+
+Bibliothèque avec une classe pour les graphes pondérés et des fonctions pour faire tourner l'algorithme de Dijkstra.
+"""
+
+class GrapheP:
+    """Graphe pondérée."""
+    def __init__(self):
+        ...
+        
+def calcule_distance(depart, graphe):
+    ...
+    
+def calcule_chemin(depart, graphe):
+    ...
