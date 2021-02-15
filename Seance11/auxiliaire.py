44cf5ddc7bdf0c04858de78e7f7c55d64812e4dd
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2021-02-15 10:54:34 +0100

    MAJ seance 11

--- Seance11/auxiliaire.py
+++ Seance11/auxiliaire.py
@@ -9,4 +9,10 @@ def enleve_sauts_de_ligne(texte: str) -> str:
 
 def convertit_majuscules(texte: str) -> str:
     """Transforme les majuscules en minuscules."""
+    return texte.lower()
+
+def supprime_caracteres_speciaux(texte: str) -> str:
+    """Enleve les non minuscules ou espace."""
     ...
+    
+# On pourra utiliser la méthode join et générer la liste des caractères admis
--- Seance11/test_auxiliaire.py
+++ Seance11/test_auxiliaire.py
@@ -2,7 +2,10 @@
 
 Tests de la bibliothèque auxiliaire.
 """
-from auxiliaire import (enleve_sauts_de_ligne,)
+from auxiliaire import (
+    enleve_sauts_de_ligne,
+    convertit_majuscules,
+)
 
 
 def test_enleve_sauts_de_ligne():
@@ -14,4 +17,18 @@ Ligne2.
 """
     sortie = "Phrase1.  Ligne2."
     calculee = enleve_sauts_de_ligne(entree)
+    assert calculee == sortie
+    
+def test_convertit_majuscules():
+    """Test."""
+    entree = "ABcdEfGh,. "
+    sortie = "abcdefgh,. "
+    calculee = convertit_majuscules(entree)
+    assert calculee == sortie
+    
+def test_supprime_caracteres_speciaux():
+    """Test."""
+    entree = ...
+    sortie = ...
+    calculee = ...
     assert calculee == sortie
